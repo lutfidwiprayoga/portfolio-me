542a233f0539248a212b9da481f9226ac7a99f2d
lutfidwiprayoga <lutfidwiprayoga00@gmail.com>
2023-06-05 16:45:11 +0700

    update dark Mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode: 'class',
   theme: {
-    container:{
-      center:true,
-      padding:'16px',
+    container: {
+      center: true,
+      padding: '16px',
     },
     extend: {
-      colors:{
-        primary:'#0ea5e9',
-        secondary:'#14b8a6',
-        danger:'#ef4444',
-        warning:'#eab308',
-        light:'#f8fafc',
-        dark:'#020617',
+      colors: {
+        primary: '#0ea5e9',
+        secondary: '#14b8a6',
+        danger: '#ef4444',
+        warning: '#eab308',
+        light: '#f8fafc',
+        dark: '#020617',
       },
     },
   },
